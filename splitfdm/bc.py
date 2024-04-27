713d0c8157220333cad023fc934969a85d38a886
Pavan BG <gpbabc@yahoo.com>
2024-04-27 20:48:14 +0530

    dirichlet and neumann first cut, visualize only interior

--- splitfdm/bc.py
+++ splitfdm/bc.py
@@ -1,4 +1,5 @@
 from .domain import Domain
+from .constants import btype, btype_map
 
 
 def apply_BC(d: Domain, v: str, bc: str = "periodic", xmin=0.0, xmax=1.0):
@@ -30,42 +31,97 @@ def apply_BC(d: Domain, v: str, bc: str = "periodic", xmin=0.0, xmax=1.0):
     # Find index of component
     idx = d.component_index(v)
 
+    # Common values used in all types
+    ilo = d.ilo()
+    ihi = d.ihi()
+
+    lb, rb = d.boundaries()
+
     if bc == "periodic":
-        lb, rb = d.boundaries()
-
-        # Get interior indices
-        ilo = d.ilo()
-        ihi = d.ihi()
-
         # left boundary
-        # Ghost cells are the rightmost elements (same order)
+        # Ghost cells are (right to left) the rightmost elements (same order)
         for i, b in enumerate(lb):
-            b.set_x(xmin - (xmax - cells[ihi - (i + 1)].x()))
+            shift = xmax - cells[ihi - (i + 1)].x()
+            b.set_x(xmin - shift)
             b.set_value(idx, cells[ihi - (i + 1)].value(idx))
 
         # right boundary
-        # Ghost cells are the leftmost elements (same order)
+        # Ghost cells are (left to right) the leftmost elements (same order)
         for i, b in enumerate(rb):
-            b.set_x(xmax + (cells[(i + 1) + ilo].x() - xmin))
+            shift = cells[(i + 1) + ilo].x() - xmin
+            b.set_x(xmax + shift)
             b.set_value(idx, cells[(i + 1) + ilo].value(idx))
 
     elif bc == "outflow":
-        # Find the lowest and highest interior indices
-        ilo = d.ilo()
-        ihi = d.ihi()
-
-        lb, rb = d.boundaries()
         # left boundary
-        # Ghost cells are the leftmost interior element
         for i, b in enumerate(lb):
-            b.set_x(xmin - (xmax - cells[ihi - (i + 1)].x()))
+            # The shift mirrors the interior on the same side
+            shift = cells[(i + 1) + ilo].x() - xmin
+            b.set_x(xmin - shift)
+            # Value same as leftmost interior
             b.set_value(idx, cells[ilo].value(idx))
 
         # right boundary
-        # Ghost cells are the leftmost elements (same order)
         for i, b in enumerate(rb):
-            b.set_x(xmax + (cells[(i + 1) + ilo].x() - xmin))
-            b.set_value(idx, cells[ihi].value(idx))
+            # The shift mirrors the interior on the same side
+            shift = xmax - cells[ihi - (i + 1)].x()
+            b.set_x(xmax + shift)
+            # Value same as extrapolated from interior
+            dy = cells[ihi].value(idx) - cells[ihi - 1].value(idx)
+            dx = cells[ihi].x() - cells[ihi - 1].x()
+            delta_x = cells[ihi + (i + 1)].x() - cells[ihi + i].x()
 
+            b.set_value(idx, cells[ihi + i].value(idx) + (dy/dx) * delta_x)
+
+    # Dictionary-based boundary conditions
+    # Dirichlet and Neumann BCs require additional values also
+    elif isinstance(bc, dict) and len(bc.keys()) == 1:
+        bctype = list(bc.keys())[0]
+
+        if bctype == "neumann":
+            # left boundary
+            neumann_value = bc[bctype][btype_map[btype.LEFT]]
+            for i, b in enumerate(lb):
+                # The shift mirrors the interior on the same side
+                shift = cells[(i + 1) + ilo].x() - xmin
+                b.set_x(xmin - shift)
+
+                # Cell width to the left
+                dx = cells[ilo - i].x() - cells[ilo - (i + 1)].x()
+                b.set_value(
+                    idx, cells[ilo - i].value(idx) - neumann_value * dx)
+
+            # right boundary
+            neumann_value = bc[bctype][btype_map[btype.RIGHT]]
+            for i, b in enumerate(rb):
+                # The shift mirrors the interior on the same side
+                shift = xmax - cells[ihi - (i + 1)].x()
+                b.set_x(xmax + shift)
+
+                # Cell width to the right
+                dx = cells[ihi + (i + 1)].x() - cells[ihi + i].x()
+                b.set_value(
+                    idx, cells[ihi + i].value(idx) + neumann_value * dx)
+
+        elif bctype == "dirichlet":
+            # left boundary
+            dirichlet_value = bc[bctype][btype_map[btype.LEFT]]
+            for i, b in enumerate(lb):
+                # The shift mirrors the interior on the same side
+                shift = cells[(i + 1) + ilo].x() - xmin
+                b.set_x(xmin - shift)
+
+                b.set_value(idx, dirichlet_value)
+
+            # right boundary
+            dirichlet_value = bc[bctype][btype_map[btype.RIGHT]]
+            for i, b in enumerate(rb):
+                # The shift mirrors the interior on the same side
+                shift = xmax - cells[ihi - (i + 1)].x()
+                b.set_x(xmax + shift)
+
+                b.set_value(idx, dirichlet_value)
+        else:
+            raise NotImplementedError
     else:
         raise NotImplementedError
