644049e3f1818869fa055053cffb229a9a87eb3d
Pavan BG <gpbabc@yahoo.com>
2024-04-28 11:23:37 +0530

    released v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="SplitFDM",
-    version="0.1.1",
+    version="0.1.2",
     description="1D Finite-Difference Split Newton Solver",
     url="https://github.com/gpavanb1/SplitFDM",
     author="gpavanb1",
